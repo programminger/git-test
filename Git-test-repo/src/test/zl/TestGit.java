628ca76e96ce118b88e700af62ecbe73dfa2c3e8
zlcoder <599660411@qq.com>
2019-07-16 23:16:56 +0800

    提交一个测试到GIT中

---
+++ Git-test-repo/src/test/zl/TestGit.java
@@ -0,0 +1,16 @@
+package test.zl;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello this is my first test-Git");
+        System.out.println("hello this is my first test-Git");
+        System.out.println("hello this is my first test-Git");
+        System.out.println("hello this is my first test-Git");
+    }
+    public void test1() {
+        int i = 9;
+        i += 10;
+        System.out.println(i);
+    }
+}
+
